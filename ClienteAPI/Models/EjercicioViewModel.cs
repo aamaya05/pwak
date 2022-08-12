77423060da1bfd2be809efbed26d4e913a2630a8
porthos-15 <kevinesquivel115@gmail.com>
2022-08-12 16:32:40 -0600

    VISTA_EJERCICIO

---
+++ ClienteAPI/Models/EjercicioViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClienteAPI.Models
+{
+    public class EjercicioViewModel
+    {
+        
+      
+        public int IdEjercicio { get; set; }
+        [Display(Name = "Nombre del ejercicio ")]
+        public string NombreEjercicio { get; set; } = null!;
+
+        public string Descripcion { get; set; } = null!;
+    }
+}
